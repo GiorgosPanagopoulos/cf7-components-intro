a09f7dd46b5899b6c0417823b3c8394125033d1d
G. Panagopoulos <panagopoulosgevrgios@gmail.com>
2025-05-31 12:09:41 +0300

    update

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,14 @@
 import Layout from "./components/Layout.tsx";
 // import ClassComponentWithState from "./components/ClassComponentWithState.tsx";
 // import FunctionalComponentWithState from "./components/FunctionalComponentWithState.tsx";
-import Counter from "./components/Counter.tsx";
+// import Counter from "./components/Counter.tsx";
+import NameChanger from "./components/NameChanger.tsx";
+// import CounterWithMoreStates from "./components/CounterWithMoreStates.tsx";
+// import CounterWithCustomHook from "./components/CounterWithCustomHook.tsx";
+// import CounterAdvanced from "./components/CounterAdvanced.tsx";
+// import CounterAdvancedWithCustomHook from "./components/CounterAdvancedWithCustomHook";
+// import CounterWithReducer from "./components/CounterWithReducer.tsx";
+// import Todo from "./components/Todo/Todo.tsx";
 
 function App() {
 
@@ -24,8 +31,15 @@ function App() {
 
                 {/*<ClassComponentWithState/>*/}
                 {/*<FunctionalComponentWithState/>*/}
-                <Counter/>
+                {/*<Counter/>*/}
+                <NameChanger/>
+                {/*<CounterWithMoreStates/>*/}
+                {/*<CounterAdvanced/>*/}
+                {/*<CounterWithCustomHook/>*/}
+                {/*<CounterAdvancedWithCustomHook/>*/}
 
+                {/*<CounterWithReducer/>*/}
+                {/*<Todo/>*/}
 
             </Layout>
         </>
